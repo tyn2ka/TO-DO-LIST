81ba7ef5acbc822ebefd189323661d68308db990
Martyna Budnik <budnik.martyna@gmail,com>
2021-08-05 19:37:11 +0200

    Map

--- src/main/java/Calendar.java
+++ src/main/java/Calendar.java
@@ -1,10 +1,14 @@
 import java.time.DayOfWeek;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Calendar {
     private static Object Month;
+    private static String day;
 
     @SuppressWarnings("ControlFlowStatementWithoutBraces")
     public static void main(String[] args) {
@@ -16,117 +20,115 @@ public class Calendar {
 
         String end = "no";
 
-            switch (lt.getMonth()) {
-                case JANUARY:
-                    dayNumber = 31;
-                    System.out.println("Hello in TODO list in January");
+        switch (lt.getMonth()) {
+            case JANUARY:
+                dayNumber = 31;
+                System.out.println("Hello in TODO list in January");
+                break;
+            case FEBRUARY:
+                boolean leapYear = lt.isLeapYear();
+                if (leapYear == true) {
+                    dayNumber = 29;
+                    System.out.println("Its Leap Year" + "Hello in TODO list in February");
+                } else {
+                    dayNumber = 28;
+                    System.out.println("Hello in TODO list in February");
                     break;
-                case FEBRUARY:
-                    boolean leapYear = lt.isLeapYear();
-                    if (leapYear == true) {
-                        dayNumber = 29;
-                        System.out.println("Its Leap Year" + "Hello in TODO list in February");
-                    } else {
-                        dayNumber = 28;
-                        System.out.println("Hello in TODO list in February");
-                        break;
-                    }
-                case MARCH:
-                    dayNumber = 31;
-                    System.out.println("Hello in TODO list in March");
-                    break;
-                case APRIL:
-                    dayNumber = 30;
-                    System.out.println("Hello in TODO list in April");
-                    break;
-                case MAY:
-                    dayNumber = 31;
+                }
+            case MARCH:
+                dayNumber = 31;
+                System.out.println("Hello in TODO list in March");
+                break;
+            case APRIL:
+                dayNumber = 30;
+                System.out.println("Hello in TODO list in April");
+                break;
+            case MAY:
+                dayNumber = 31;
 
 
-
-                    System.out.println("Hello in TODO list in May");
-                    break;
-                case JUNE:
-                    dayNumber = 30;
-                    System.out.println("Hello in TODO list in June");
-                    break;
-                case JULY:
-                    dayNumber = 31;
-                    System.out.println("Hello in TODO list in July");
-                    break;
-                case AUGUST:
-                    dayNumber = 31;
-                    System.out.println("Hello in TODO list in August");
-                    break;
-                case SEPTEMBER:
-                    dayNumber = 30;
-                    System.out.println("Hello in TODO list in September");
-                    break;
-                case OCTOBER:
-                    dayNumber = 31;
-                    System.out.println("Hello in TODO list in October");
-                    break;
-                case NOVEMBER:
-                    dayNumber = 30;
-                    System.out.println("Hello in TODO list in November");
-                    break;
-                case DECEMBER:
-                    dayNumber = 31;
-                    System.out.println("Hello in TODO list in December");
-                    break;
-            }
-        Scanner scanner= new Scanner(System.in);
+                System.out.println("Hello in TODO list in May");
+                break;
+            case JUNE:
+                dayNumber = 30;
+                System.out.println("Hello in TODO list in June");
+                break;
+            case JULY:
+                dayNumber = 31;
+                System.out.println("Hello in TODO list in July");
+                break;
+            case AUGUST:
+                dayNumber = 31;
+                System.out.println("Hello in TODO list in August");
+                break;
+            case SEPTEMBER:
+                dayNumber = 30;
+                System.out.println("Hello in TODO list in September");
+                break;
+            case OCTOBER:
+                dayNumber = 31;
+                System.out.println("Hello in TODO list in October");
+                break;
+            case NOVEMBER:
+                dayNumber = 30;
+                System.out.println("Hello in TODO list in November");
+                break;
+            case DECEMBER:
+                dayNumber = 31;
+                System.out.println("Hello in TODO list in December");
+                break;
+        }
+        Scanner scanner = new Scanner(System.in);
         System.out.println("How many tasks do you want to write?");
-        int numberTasks= scanner.nextInt();
+        int numberTasks = scanner.nextInt();
 
-            DayOfWeek d = lt.getDayOfWeek();
+        DayOfWeek d = lt.getDayOfWeek();
 
-            switch (lt.getDayOfWeek()) {
+        switch (lt.getDayOfWeek()) {
 
-                case MONDAY:
-                    System.out.println("Its Monday");
-                    System.out.println("Write tasks for Monday");
-                    break;
-                case TUESDAY:
-                    System.out.println("Its Tuesday");
-                    System.out.println("How many tasks do you want to write");
-                    break;
-                case WEDNESDAY:
-                    System.out.println("Its Wenesday");
-                    System.out.println("Write tasks for Wednesday");
-                    break;
-                case THURSDAY:
-                    System.out.println("Its Thursday");
-                    System.out.println("Write tasks for Thursday");
-                    break;
-                case FRIDAY:
-                    System.out.println("Its Friday");
-                    System.out.println("Write tasks for Friday");
-                    break;
-                case SATURDAY:
-                    System.out.println("Its Saturday");
-                    System.out.println("Write tasks for Saturday");
-                    break;
-                case SUNDAY:
-                    System.out.println("Its Sunday");
-                    System.out.println("Write tasks for Sunday");
-                    break;
-            }
-
-            for (int i=0; i<numberTasks;i++);{
-            Scanner Tasks = new Scanner(System.in);
-
-            String phrase1;
-            phrase1 =Tasks.nextLine();
-
-
+            case MONDAY:
+                System.out.println("Its Monday");
+                System.out.println("Write tasks for Monday");
+                break;
+            case TUESDAY:
+                System.out.println("Its Tuesday");
+                System.out.println("How many tasks do you want to write");
+                break;
+            case WEDNESDAY:
+                System.out.println("Its Wenesday");
+                System.out.println("Write tasks for Wednesday");
+                break;
+            case THURSDAY:
+                System.out.println("Its Thursday");
+                System.out.println("Write tasks for Thursday");
+                break;
+            case FRIDAY:
+                System.out.println("Its Friday");
+                System.out.println("Write tasks for Friday");
+                break;
+            case SATURDAY:
+                System.out.println("Its Saturday");
+                System.out.println("Write tasks for Saturday");
+                break;
+            case SUNDAY:
+                System.out.println("Its Sunday");
+                System.out.println("Write tasks for Sunday");
+                break;
         }
 
+        Scanner Tasks = null;
+        Map<Integer, String> tasks = new HashMap<>();
+        for (int i = 0; i < numberTasks; i++) {
+            Tasks = new Scanner(System.in);
+            Integer a = i;
+            String phrase1;
+            phrase1 = Tasks.nextLine();
 
-
-
-
-
+            tasks.put(a,phrase1);
+        }
+        for (Map.Entry<Integer,String>m: tasks.entrySet()){
+            System.out.println(m.getKey()+ " " + m.getValue());
+        }
 
 
 
